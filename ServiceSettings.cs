2b34c88611019039bdf32e16fbb68c6663b93ff0
alvaro alves <alvaroalves87@gmail.com>
2021-07-11 18:27:06 -0300

    Ajustes na inicialização e criação de servicesettings class

---
+++ ServiceSettings.cs
@@ -0,0 +1,9 @@
+namespace Dotnet5microservices
+{
+    class ServiceSettings
+    {
+        public string HostCorreios { get; set; }
+        //URL: viacep.com.br/ws/01001000/json/
+        
+    }
+}
